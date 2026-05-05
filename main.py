6f4607ea101dd39a2488567425acfa4cb3199c1f
NihalW-24 <nihalwaseem123@gmail.com>
2026-05-05 15:28:30 +0100

    Added pokemon height and weight for 3 different pokemons to json file

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ def get_pokemon_data(pokemon_name):
     response  = requests.get(pokemon_url) 
     data = response.json()
     print(response.status_code)
-    
-    with open ("pokemon.json", "w") as file : 
-        json.dump (data,file, indent=4 )
-    
 
-    
+    pokemon_height_weight = {
+        "name": data["name"],
+        "height": data["height"],
+        "weight": data["weight"]
+    }
 
+    print(pokemon_height_weight)
+    
+    with open ("pokemon.json", "a") as file : 
+        json.dump (pokemon_height_weight,file, indent=4 )
+    
 get_pokemon_data("pikachu")
+get_pokemon_data("bulbasaur")
 
 
